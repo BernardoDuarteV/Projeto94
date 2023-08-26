ea828f28a71afc115a760421c9b87443217326b9
BernardoDuarteV <128868900+BernardoDuarteV@users.noreply.github.com>
2023-08-26 11:24:49 -0300

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDYAfb1saXOyOV7wXK-nrnr1N7s-EWeEI",
+    authDomain: "projeto93-e0b34.firebaseapp.com",
+    databaseURL: "https://projeto93-e0b34-default-rtdb.firebaseio.com",
+    projectId: "projeto93-e0b34",
+    storageBucket: "projeto93-e0b34.appspot.com",
+    messagingSenderId: "435458050396",
+    appId: "1:435458050396:web:922f33555527affd02d999",
+    measurementId: "G-XN895YWVXS"
+  };
+
+  const app = initializeApp(firebaseConfig);
